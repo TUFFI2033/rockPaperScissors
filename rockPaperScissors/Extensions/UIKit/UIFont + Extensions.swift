cf4e6f49cf0b1dfd671c94e409e10687e1ee3de3
Ivan <bindiuua@gmail.com>
2023-09-24 20:17:26 +0100

    main srceen

---
+++ rockPaperScissors/Extensions/UIKit/UIFont + Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIFont + Extensions.swift
+//  rockPaperScissors
+//
+//  Created by Ivan Byndiu on 24/09/2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    //Medium
+    static func robotoMediu16() -> UIFont? {
+        UIFont.init(name: "Roboto-Medium", size: 16)
+    }
+    
+    //Bold
+    static func robotoBold36() -> UIFont? {
+        UIFont.init(name: "Roboto-Bold", size: 36)
+    }
+    
+    static func robotoBold13() -> UIFont? {
+        UIFont.init(name: "Roboto-Bold", size: 13)
+    }
+}
